4cffbb54ccdf4c38a31f0626080a4b647c38bb44
Subhajit Bokshi <52731122+Subhajitb17@users.noreply.github.com>
2025-04-16 11:26:52 +0530

    nothing

--- controllers/labellingController.js
+++ controllers/labellingController.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const storage = new Storage({
   keyFilename: process.env.GCP_KEY_FILE_PATH || // Use env variable first
-    path.join(__dirname, "../services/ship-duniya-labeling-key.json"),
+    path.join(__dirname, "../services/keys/ship-duniya-903aac2a0c77-key.json"),
   projectId: process.env.GCP_PROJECT_ID // Add project ID
 });
 
